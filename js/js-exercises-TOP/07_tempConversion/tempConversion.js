50f1a3febd83aa3415ba21868f0d634784c5230b
Animesh Kumar <kumaranimesh923@gmail.com>
2024-02-06 00:57:35 +0530

    javascript exercises, temperature conversion done

--- js/js-exercises-TOP/07_tempConversion/tempConversion.js
+++ js/js-exercises-TOP/07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  let res=((input-32)*(5/9));
+  let roundedRes = Math.round(res * 10) / 10
+  return roundedRes;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  let res=(input*(9/5)+32);
+  let roundedRes = Math.round(res * 10) / 10
+  return roundedRes;
 };
 
 // Do not edit below this line
