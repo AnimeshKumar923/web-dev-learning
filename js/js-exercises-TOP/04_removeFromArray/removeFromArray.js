5ba16e87be2e5d0f1aa3be537de01936bba7536c
Animesh Kumar <kumaranimesh923@gmail.com>
2024-02-06 23:55:03 +0530

    javascript exercises, optimized solution

--- js/js-exercises-TOP/04_removeFromArray/removeFromArray.js
+++ js/js-exercises-TOP/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function(arr, ...targetArr) {
     for(let j=0; j<targetArr.length; j++){
-        for(let i=0; i<arr.length; i++){
+        for(let i=arr.length; i>=0; i--){
             if(arr[i]===targetArr[j]){
                 let index=i;
                 arr.splice(index, 1);
@@ -10,5 +10,6 @@ const removeFromArray = function(arr, ...targetArr) {
     return arr;
 };
 
+console.log(removeFromArray([3,2,2,1,1], 2,1))
 // Do not edit below this line
 module.exports = removeFromArray;
